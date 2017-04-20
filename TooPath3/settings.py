d90a8e0448fa285339dbe00d68389fbf351dafae
albert.diaz.benitez <albert.diaz.benitez@fib.upc.edu>
2017-04-20 12:57:11 +0200

    PostrgesSQL DB added to Settings

--- TooPath3/settings.py
+++ TooPath3/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'TooPath3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'toopath',
+        'USER': 'django',
+        'PASSWORD': 'toopath3',
+        'HOST': '127.0.0.1',
+        'PORT': '15432',
     }
 }
 
