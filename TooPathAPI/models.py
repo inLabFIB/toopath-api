6f079d6830c4715dbe50905b6617454b59b75257
Albert Diaz <albert.diaz.benitez@fib.upc.edu>
2017-04-24 10:55:47 +0200

    Device and Locations models added

--- TooPathAPI/models.py
+++ TooPathAPI/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Device(models.Model):
+    id = models.CharField(primary_key=True, max_length=20)
+
+    class Meta:
+        db_table = 'device'
+
+    def __init__(self, *args, **kwargs):
+        super(Device, self).__init__(*args, **kwargs)
+
+
+class Location(models.Model):
+    point = models.PointField()
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'location'
+
+    def __init__(self, *args, **kwargs):
+        super(Location, self).__init__(*args, **kwargs)
