c9481e0a03d586fab1cf30870d0e820ec74d7aec
Albert Diaz <albert.diaz.benitez@fib.upc.edu>
2017-06-14 13:32:45 +0200

    allowed production hosts updated

--- TooPath3/settings/production.py
+++ TooPath3/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['147.83.153.201', '127.0.0.1']
+ALLOWED_HOSTS = ['147.83.153.201', '147.83.153.208', '127.0.0.1']
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
