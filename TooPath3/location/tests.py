1e2983f37f047467483308319770fe57d4dfbfdf
Albert Diaz <albert.diaz.benitez@fib.upc.edu>
2017-07-03 12:20:48 +0200

    POST actualLocation tests added

--- TooPath3/location/tests.py
+++ TooPath3/location/tests.py
@@ -8,6 +8,10 @@ DATA_LOCATION = {
     'latitude': 40.1234,
     'longitude': 2.1234
 }
+INVALID_DATA_LOCATION = {
+    'lat': 40.1234,
+    'lon': 2.1234
+}
 
 
 class ActualLocationTests(APITestCase):
@@ -15,11 +19,24 @@ class ActualLocationTests(APITestCase):
         Device.objects.create(did=1, name='car', ip_address='0.0.0.0', device_type='ad', device_privacy='pr')
 
     """
-    POST /devices/:id/location
+    POST /devices/:id/actualLocation
     """
 
     def test_given_existing_device__when_post_device_location_with_existing_device_id__then_return_created(self):
         factory = APIRequestFactory()
-        request = factory.post('/devices/1/location', DATA_LOCATION, format='json')
-        response = device_location(request, id=1)
+        request = factory.post('/devices/1/actualLocation', DATA_LOCATION, format='json')
+        response = device_actual_location(request, id=1)
         self.assertEqual(response.status_code, HTTP_201_CREATED)
+
+    def test_given_existing_device__when_post_device_location_with_existent_id_and_invalid_data__then_return_bad_request(
+            self):
+        factory = APIRequestFactory()
+        request = factory.post('/devices/1/actualLocation', INVALID_DATA_LOCATION, format='json')
+        response = device_actual_location(request, id=1)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
+
+    def test_given_existing_device__when_get_device_location__with_non_existent_device_id__then_return_not_found(self):
+        factory = APIRequestFactory()
+        request = factory.post('/devices/1/actualLocation', DATA_LOCATION, format='json')
+        response = device_actual_location(request, id=10)
+        self.assertEqual(response.status_code, HTTP_404_NOT_FOUND)
