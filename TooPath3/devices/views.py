aac7c114055a44a4681a0afb264bd66acede1eb3
Albert Diaz <albert.diaz.benitez@fib.upc.edu>
2017-07-24 10:55:02 +0200

    object owner permission working

--- TooPath3/devices/views.py
+++ TooPath3/devices/views.py
@@ -1,27 +1,33 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import *
+from rest_framework.views import APIView
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
+from TooPath3.devices.permissions import IsOwnerOrReadOnly
 from TooPath3.devices.serializers import DeviceSerializer
 from TooPath3.models import Device
 
 
-@api_view(['GET', 'PUT'])
-@authentication_classes((JSONWebTokenAuthentication, SessionAuthentication, BasicAuthentication),)
-@permission_classes((IsAuthenticated,))
-def device_detail(request, id):
-    device = get_object_or_404(Device, pk=id)
+class DeviceDetail(APIView):
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication, BasicAuthentication,)
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
 
-    if request.method == 'GET':
+    def get_object(self, pk):
+        obj = get_object_or_404(Device, pk=pk)
+        self.check_object_permissions(self.request, obj=obj)
+        return obj
+
+    def get(self, request, pk, format=None):
+        device = self.get_object(pk)
         serializer = DeviceSerializer(device)
         return Response(serializer.data, status=HTTP_200_OK)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        device = self.get_object(pk)
         data = JSONParser().parse(request)
         if 'name' not in data:
             data['name'] = device.name
