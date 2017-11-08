1b298fc0c174063eed0fd1f55da8846fba9cb869
Albert Díaz Benitez <albert.diaz.benitez@gmail.com>
2017-11-08 13:04:48 +0100

    POST tracks permissions added

--- TooPath3/tracks/tests.py
+++ TooPath3/tracks/tests.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.hashers import make_password
-from rest_framework.status import HTTP_404_NOT_FOUND
+from rest_framework.status import *
 from rest_framework.test import APITestCase, APIClient
 
-from TooPath3.models import CustomUser
+from TooPath3.models import CustomUser, Device
 from TooPath3.users.utils import generate_token_for_testing
 
 
@@ -16,3 +16,9 @@ class PostTracksCase(APITestCase):
     def test_return_404_status_when_device_not_exists(self):
         response = self.client.post('/devices/100/tracks/', {})
         self.assertEqual(HTTP_404_NOT_FOUND, response.status_code)
+
+    def test_return_403_status_when_user_doesnt_have_permissions(self):
+        owner = CustomUser.objects.create(username='owner', password=make_password('password'))
+        device = Device.objects.create(name='device_test', device_type='ad', device_privacy='pr', owner=owner)
+        response = self.client.post('/devices/' + str(device.did) + '/tracks/', {})
+        self.assertEqual(HTTP_403_FORBIDDEN, response.status_code)
--- TooPath3/tracks/views.py
+++ TooPath3/tracks/views.py
@@ -1,13 +1,20 @@
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-from TooPath3.models import Track
+from TooPath3.devices.permissions import IsOwnerOrReadOnly
+from TooPath3.models import Device
 
 
 class TracksList(APIView):
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication, BasicAuthentication,)
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
+
     def get_object(self, pk):
-        obj = get_object_or_404(Track, pk=pk)
+        obj = get_object_or_404(Device, pk=pk)
         self.check_object_permissions(self.request, obj=obj)
         return obj
 
