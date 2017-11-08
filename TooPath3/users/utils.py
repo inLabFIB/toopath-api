0c6a9ed6490032f704b16c51cfa59ab0f62f81e7
Albert Díaz Benitez <albert.diaz.benitez@gmail.com>
2017-11-08 12:28:43 +0100

    get_token_for_testing method added on user utils

--- TooPath3/users/utils.py
+++ TooPath3/users/utils.py
@@ -1,6 +1,15 @@
+from rest_framework_jwt.settings import api_settings
+
 from TooPath3.models import CustomUser
 
 
 def get_jwt_secret(user):
     custom_user = CustomUser.objects.get(pk=user.pk)
     return custom_user.jwt_secret
+
+
+def generate_token_for_testing(user):
+    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+    payload = jwt_payload_handler(user)
+    return jwt_encode_handler(payload)
